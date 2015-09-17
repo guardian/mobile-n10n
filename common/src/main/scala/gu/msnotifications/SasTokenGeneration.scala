5c695fb68c8f76a19565d99c284e9265c04f6985
David Furey <david.furey@guardian.co.uk>
2015-09-17 10:22:13 +0100

    Always base64 encode topics

--- common/src/main/scala/gu/msnotifications/SasTokenGeneration.scala
+++ common/src/main/scala/gu/msnotifications/SasTokenGeneration.scala
@@ -7,6 +7,7 @@ import javax.crypto.spec.SecretKeySpec
 import org.apache.commons.codec.binary.Base64
 
 import scala.util.control.NonFatal
+import scalaz.\/
 
 object SasTokenGeneration {
 
@@ -16,26 +17,21 @@ object SasTokenGeneration {
    * From [[https://github.com/Azure/azure-notificationhubs-java-backend/blob/master/NotificationHubs/src/com/windowsazure/messaging/NamespaceManager.java#L233]]
    * Licensed under Apache License 2.0 - [[https://github.com/Azure/azure-notificationhubs-java-backend/blob/master/LICENSE]]
    */
-  def generateSasToken(sasKeyName: String, sasKeyValue: String, uri: String): String = {
-    try {
-      val targetUri = URLEncoder.encode(uri.toLowerCase, "UTF-8").toLowerCase
-      val expires = (System.currentTimeMillis + (expiresInMins * 60 * 1000)) / 1000
-      val rawHmac = {
-        val toSign = s"$targetUri\n$expires"
-        val keyBytes = sasKeyValue.getBytes("UTF-8")
-        val signingKey = new SecretKeySpec(keyBytes, "HmacSHA256")
-        val mac = Mac.getInstance("HmacSHA256")
-        mac.init(signingKey)
-        mac.doFinal(toSign.getBytes("UTF-8"))
-      }
-      val signature = URLEncoder.encode(Base64.encodeBase64String(rawHmac), "UTF-8")
-      s"SharedAccessSignature sr=$targetUri&sig=$signature&se=$expires&skn=$sasKeyName"
-    } catch {
-      case NonFatal(e) =>
-        throw new SasTokenGenerationException(e)
+  def generateSasToken(sasKeyName: String, sasKeyValue: String, uri: String) = {
+    val targetUri = URLEncoder.encode(uri.toLowerCase, "UTF-8").toLowerCase
+    val expires = (System.currentTimeMillis + (expiresInMins * 60 * 1000)) / 1000
+    val rawHmac = {
+      val toSign = s"$targetUri\n$expires"
+      val keyBytes = sasKeyValue.getBytes("UTF-8")
+      val signingKey = new SecretKeySpec(keyBytes, "HmacSHA256")
+      val mac = Mac.getInstance("HmacSHA256")
+      mac.init(signingKey)
+      mac.doFinal(toSign.getBytes("UTF-8"))
     }
+    val signature = URLEncoder.encode(Base64.encodeBase64String(rawHmac), "UTF-8")
+    s"SharedAccessSignature sr=$targetUri&sig=$signature&se=$expires&skn=$sasKeyName"
   }
 
 }
 
-class SasTokenGenerationException(e: Throwable) extends RuntimeException(e)
+case class SasTokenError(e: Throwable)
