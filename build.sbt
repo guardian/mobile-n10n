fbaf83eb0828036aecf98ed2cd31c98d51c0ac9f
David Furey <david.furey@guardian.co.uk>
2015-09-18 17:10:52 +0100

    Use guardian configuration library

--- build.sbt
+++ build.sbt
@@ -1,11 +1,16 @@
 import com.gu.riffraff.artifact.RiffRaffArtifact.autoImport._
 
 lazy val common = project.settings(
+  resolvers ++= Seq(
+    "Guardian GitHub Releases" at "http://guardian.github.com/maven/repo-releases",
+    "Guardian GitHub Snapshots" at "http://guardian.github.com/maven/repo-snapshots"
+  ),
   libraryDependencies ++= Seq(
     json,
     ws,
     "com.microsoft.azure" % "azure-servicebus" % "0.7.0",
-    "org.scalaz" %% "scalaz-core" % "7.1.0"
+    "org.scalaz" %% "scalaz-core" % "7.1.0",
+    "com.gu" %% "configuration" %  "4.1"
   )
 )
 
--- notification/app/services/NotificationConfiguration.scala
+++ notification/app/services/NotificationConfiguration.scala
@@ -1,15 +1,20 @@
 package services
 
-import javax.inject.Inject
-
-import play.api.Configuration
-
 import scala.concurrent.ExecutionContext
+import com.gu.conf.ConfigurationFactory
+import scalaz.\/
+import scalaz.std.option.optionSyntax._
 
 case class ErrorMessage(message: String)
 
-final class NotificationConfiguration @Inject()(configuration: Configuration)
-  (implicit executionContext: ExecutionContext) {
+final class NotificationConfiguration(implicit executionContext: ExecutionContext) {
 
+  private lazy val conf = ConfigurationFactory.getConfiguration(
+    applicationName = "notification",
+    webappConfDirectory = "gu-conf"
+  )
+
+  private def getConfigurationProperty(name: String): ErrorMessage \/ String =
+    conf.getStringProperty(name) \/> ErrorMessage(s"Could not find property $name")
 
 }
