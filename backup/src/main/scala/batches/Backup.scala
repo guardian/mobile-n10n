83b620952e2c3bd0a28816c27a4ee47af789f358
Alexandre Dufournet <alexandre.dufournet@gmail.com>
2015-10-07 10:38:43 +0100

    cleanup old files and inject configuration

--- backup/src/main/scala/batches/Backup.scala
+++ backup/src/main/scala/batches/Backup.scala
@@ -5,32 +5,25 @@ import javax.inject.Inject
 
 import com.microsoft.azure.storage.CloudStorageAccount
 import com.microsoft.azure.storage.blob.SharedAccessBlobPermissions._
-import com.microsoft.azure.storage.blob.SharedAccessBlobPolicy
+import com.microsoft.azure.storage.blob._
 import gu.msnotifications.{NotificationHubJobType, _}
 import org.joda.time.{DateTime, DateTimeZone}
 import play.api.libs.ws.WSClient
-import play.api.{Logger, Application}
+import play.api.Logger
+import collection.JavaConversions._
 
 import scala.concurrent.{ExecutionContext, Future}
 import scalaz.{-\/, \/-}
 
-class Backup @Inject() (app: Application, ws: WSClient)(implicit ec: ExecutionContext) extends Batch {
-
-  def getConfigString(key: String): String = app.configuration.getString(key)
-    .getOrElse(throw new RuntimeException(s"key $key not found in configuration"))
-
-  val storageConnectionString = getConfigString("azure.storageConnectionString")
-  val containerName = getConfigString("azure.storageContainerName")
-  val notificationHubConnection = NotificationHubConnection(
-    getConfigString("azure.url"),
-    getConfigString("azure.secretKeyName"),
-    getConfigString("azure.secretKey"))
+class Backup @Inject() (conf: BackupConfiguration, ws: WSClient)(implicit ec: ExecutionContext) extends Batch {
 
   def execute(): Future[Unit] = {
-    val storageAccount = CloudStorageAccount.parse(storageConnectionString)
+    val storageAccount = CloudStorageAccount.parse(conf.storageConnectionString)
     val blobClient = storageAccount.createCloudBlobClient()
-    val container = blobClient.getContainerReference(containerName)
+    val container = blobClient.getContainerReference(conf.containerName)
     container.createIfNotExists()
+
+    cleanup(container)
 
     val sharedAccessPolicy = new SharedAccessBlobPolicy()
     sharedAccessPolicy.setPermissions(util.EnumSet.of(READ, WRITE, LIST))
@@ -44,12 +37,34 @@ class Backup @Inject() (app: Application, ws: WSClient)(implicit ec: ExecutionCo
     )
 
     Logger.info(notificationJob.toXml.toString())
-    val hubClient = new NotificationHubClient(notificationHubConnection, ws)
+    val hubClient = new NotificationHubClient(conf.notificationHubConnection, ws)
     hubClient.submitNotificationHubJob(notificationJob).map {
       case \/-(result) => result.items.foreach { job =>
         Logger.info(s"Success, job created with id ${job.jobId}\n$job")
       }
       case -\/(failure) => Logger.error(failure.toString)
+    }
+  }
+
+  private def cleanup(container: CloudBlobContainer): Unit = {
+
+    def listAllFiles(directory: CloudBlobDirectory): List[CloudBlob] = {
+      // toList to force the lazy list to be evaluated
+      val directoryContent = directory.listBlobs().toList
+      directoryContent.flatMap {
+        case blob: CloudBlob => List(blob)
+        case dir: CloudBlobDirectory => listAllFiles(dir)
+        case _ => Nil
+      }
+    }
+
+    val tooOld = DateTime.now().minusDays(conf.backupRetention).toDate
+    val directory = container.getDirectoryReference(conf.directoryName)
+    val allBlobs = listAllFiles(directory)
+    val blobsToDelete = allBlobs.filter(_.getProperties.getLastModified.before(tooOld))
+    blobsToDelete.foreach { blob =>
+      blob.deleteIfExists()
+      Logger.info(s"deleted old backup: ${blob.getName}")
     }
   }
 }
