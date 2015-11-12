255d90b2b007e329f7e60864ceece78f46533f46
Alexandre Dufournet <alexandre.dufournet@gmail.com>
2015-11-12 18:19:35 +0000

    - encode in base16 instead of 64 so microsoft accepts tags - No space allowed in the tags, not even to separate them.

--- common/src/main/scala/gu/msnotifications/package.scala
+++ common/src/main/scala/gu/msnotifications/package.scala
@@ -1,16 +1,16 @@
 package gu
 
-import java.util.Base64
-
-import models.{TopicType, Topic, UserId}
+import models.{Topic, UserId}
 
 package object msnotifications {
 
-  private def encode(string: String): String =
-    Base64.getUrlEncoder.encodeToString(string.getBytes("UTF-8"))
+  private def encodeBase16(string: String): String =
+    string.getBytes("UTF-8").map("%02X".format(_)).mkString.toLowerCase
 
-  private def decode(string: String): String =
-    new String(Base64.getUrlDecoder.decode(string), "UTF-8")
+  private def decodeBase16(string: String): String = {
+    val bytes = string.grouped(2).map(unit => java.lang.Byte.parseByte(unit, 16))
+    new String(bytes.toArray, "UTF-8")
+  }
 
   case class Tag(encodedTag: String)
 
@@ -18,7 +18,7 @@ package object msnotifications {
     import Tags._
 
     def fromTopic(t: Topic): Tag = {
-      Tag(s"$TopicTagPrefix${encode(t.`type`.toString)}:${encode(t.name)}")
+      Tag(s"${TopicTagPrefix}Base16:${encodeBase16(t.toString)}")
     }
 
     def fromUserId(u: UserId): Tag = {
@@ -32,29 +32,27 @@ package object msnotifications {
 
     def asSet = tags.map(_.encodedTag)
 
-    def findUserId: Option[UserId] = urisInTags
+    def findUserId: Option[UserId] = encodedTags
       .find(_.matches(UserTagRegex.regex))
       .map { case UserTagRegex(uname) => UserId(uname) }
 
-    def decodedTopics: Set[Topic] = for {
-      encodedUri <- urisInTags
-      if encodedUri.matches(TopicTagRegex.regex)
-      TopicTagRegex(tt, name) = encodedUri
-      topicType <- TopicType.fromString(decode(tt))
-    } yield Topic(topicType, decode(name))
+    def decodedTopics: Set[Topic] = {
+      val topics = encodedTags.collect{ case TopicTagRegex(encodedTopic) => decodeBase16(encodedTopic) }
+      topics.map(Topic.fromString).flatMap(_.toOption)
+    }
 
     def withUserId(userId: UserId) = copy(tags + Tag.fromUserId(userId))
 
     def withTopics(topics: Set[Topic]) = copy(tags ++ topics.map(Tag.fromTopic))
 
-    private[this] def urisInTags = tags.map(_.encodedTag)
+    private[this] def encodedTags = tags.map(_.encodedTag)
   }
 
   object Tags {
     val UserTagPrefix = "user:"
     val TopicTagPrefix = "topic:"
     val UserTagRegex = """user:(.*)""".r
-    val TopicTagRegex = """topic:(.*):(.*)""".r
+    val TopicTagRegex = """topic:Base16:(.*)""".r
 
     def fromStrings(tags: Set[String]) = Tags(tags.map(Tag(_)))
   }
