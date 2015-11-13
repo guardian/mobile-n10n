df9cd66dc771fd5a2674f45eae16ae9c44e1fb19
Alexandre Dufournet <alexandre.dufournet@gmail.com>
2015-11-13 11:14:50 +0000

    Add time unit

--- backup/src/main/scala/batches/BackupConfiguration.scala
+++ backup/src/main/scala/batches/BackupConfiguration.scala
@@ -10,7 +10,7 @@ class BackupConfiguration extends NotificationConfiguration("backup") {
   lazy val storageConnectionString = getConfigString("azure.storage.connectionString")
   lazy val containerName = getConfigString("azure.storage.containerName")
   lazy val directoryName = getConfigString("azure.storage.directoryName")
-  lazy val backupRetention = getConfigInt("backup.retention")
+  lazy val backupRetention = getConfigInt("backup.retentionInDays")
   lazy val notificationHubConnection = NotificationHubConnection(
     getConfigString("azure.hub.endpoint"),
     getConfigString("azure.hub.sharedAccessKeyName"),
