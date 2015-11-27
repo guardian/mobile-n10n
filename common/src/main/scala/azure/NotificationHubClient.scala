3adf627d2188584c7ab636280f6cb2e740d0018e
Alexandre Dufournet <alexandre.dufournet@gmail.com>
2015-11-27 10:54:09 +0000

    Fixing the friday mistake

--- common/src/main/scala/azure/NotificationHubClient.scala
+++ common/src/main/scala/azure/NotificationHubClient.scala
@@ -53,7 +53,7 @@ class NotificationHubClient(notificationHubConnection: NotificationHubConnection
       .withHeaders(serviceBusTags: _*)
       .post(azureWindowsPush.body)
       .map { response =>
-        if (response.status >= 200 || response.status < 300)
+        if (response.status >= 200 && response.status < 300)
           \/.right(())
         else
           \/.left(XmlParser.parseError(response))
