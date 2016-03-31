e89e3edb2af48c637f0911ec49b4e90c97f88e4b
pvighi <patricio.vighi@guardian.co.uk>
2016-03-31 13:57:04 +0100

    MAPI-1174: enable multiple keys in report

--- report/app/report/services/Configuration.scala
+++ report/app/report/services/Configuration.scala
@@ -3,6 +3,7 @@ package report.services
 import conf.NotificationConfiguration
 
 class Configuration extends NotificationConfiguration("report") {
-  lazy val apiKey = conf.getStringProperty("notifications.api.secretKey")
+  lazy val apiKeys = conf.getStringPropertiesSplitByComma("notifications.api.secretKeys")
+
   lazy val dynamoReportsTableName = getConfigString("db.dynamo.reports.table-name")
 }
