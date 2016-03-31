4781154d3aee5dd856899d35a0f8021b213c27b9
pvighi <patricio.vighi@guardian.co.uk>
2016-03-31 16:05:25 +0100

    tiny code formatting change

--- report/app/report/services/Configuration.scala
+++ report/app/report/services/Configuration.scala
@@ -4,6 +4,5 @@ import conf.NotificationConfiguration
 
 class Configuration extends NotificationConfiguration("report") {
   lazy val apiKeys = conf.getStringPropertiesSplitByComma("notifications.api.secretKeys")
-
   lazy val dynamoReportsTableName = getConfigString("db.dynamo.reports.table-name")
 }
