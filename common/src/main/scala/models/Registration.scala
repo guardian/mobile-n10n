128b5ad6987fdf091e120bee5e6a59175c8bc7e7
David Furey <david.furey@guardian.co.uk>
2016-07-29 10:41:21 +0100

    Add explanation for userId/udid rewrite

--- common/src/main/scala/models/Registration.scala
+++ common/src/main/scala/models/Registration.scala
@@ -13,6 +13,8 @@ object Registration {
 
     private val basicReader = Json.reads[Registration]
 
+    // This is for backwards compatibility with the Windows 10 app, which sends a unique device identifier but
+    // calls it a userId.
     private def replaceUserId(json: JsValue) = json.validate[JsObject] map { o =>
       (o.value.get("udid"), o.value.get("userId")) match {
         case (None, Some(userId)) => o + ("udid" -> userId)
