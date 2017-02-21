4c72be97ccd25e71e3bacd86c88b8c1a01394393
David Furey <david.furey@guardian.co.uk>
2017-02-21 09:39:24 +0000

    Fix tests

--- report/test/report/controllers/ReportIntegrationSpec.scala
+++ report/test/report/controllers/ReportIntegrationSpec.scala
@@ -104,6 +104,7 @@ class ReportIntegrationSpec(implicit ee: ExecutionEnv) extends PlaySpecification
       val configuration = mock[Configuration]
       configuration.apiKeys returns List(apiKey)
       configuration.electionRestrictedApiKeys returns List.empty
+      configuration.reportsOnlyApiKeys returns List.empty
       configuration
     }
 
