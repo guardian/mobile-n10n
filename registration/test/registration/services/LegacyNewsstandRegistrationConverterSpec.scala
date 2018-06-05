eccb71a5874c71757b4e7ab1ab0b53514a90f29c
Mark Richards <mark.richards@guardian.co.uk>
2018-06-05 17:30:33 +0100

    Fixing tests

--- registration/test/registration/services/LegacyNewsstandRegistrationConverterSpec.scala
+++ registration/test/registration/services/LegacyNewsstandRegistrationConverterSpec.scala
@@ -1,7 +1,7 @@
 package registration.services
+
 import java.util.UUID
 
-import error.NotificationsError
 import models.TopicTypes.NewsstandShard
 import models._
 import org.specs2.mutable.Specification
@@ -11,7 +11,7 @@ class LegacyNewsstandRegistrationConverterSpec extends Specification {
 
   "LegacyNewsstandRegistrationConverter" should {
     "toRegistration" in {
-      val pushTokensToShards = Map("pushToken" -> 8, "pushToken3" -> 9)
+      val pushTokensToShards = Map("pushToken" -> 7, "pushToken3" -> 0)
       val legacyNewsstandRegistrationConverter = new LegacyNewsstandRegistrationConverter(NewsstandShardConfig(10))
       pushTokensToShards.foreach {
         case (pushToken, shardExpected) =>
@@ -22,17 +22,11 @@ class LegacyNewsstandRegistrationConverterSpec extends Specification {
       ok
     }
     "toRegistration" in {
-      val pushTokensToShards = Map("pushToken" -> 8, "pushToken3" -> 9)
       val legacyNewsstandRegistrationConverter = new LegacyNewsstandRegistrationConverter(NewsstandShardConfig(10))
-      pushTokensToShards.foreach {
-        case (pushToken, shardExpected) =>
-          val inputLegacyNewsstandRegistration = LegacyNewsstandRegistration(pushToken)
-          val registrationResponse = RegistrationResponse("deviceId", Newsstand, UniqueDeviceIdentifier(UUID.randomUUID()), Set(Topic(NewsstandShard, s"newsstand-shard-$shardExpected")))
-          val outputNewstandRegistration: LegacyNewsstandRegistration = legacyNewsstandRegistrationConverter.fromResponse(inputLegacyNewsstandRegistration, registrationResponse)
-          outputNewstandRegistration must beEqualTo(inputLegacyNewsstandRegistration)
-
-      }
-      ok
+      val inputLegacyNewsstandRegistration = LegacyNewsstandRegistration("pushToken")
+      val registrationResponse = RegistrationResponse("deviceId", Newsstand, UniqueDeviceIdentifier(UUID.randomUUID()), Set(Topic(NewsstandShard, "newsstand-shard-0")))
+      val outputNewstandRegistration: LegacyNewsstandRegistration = legacyNewsstandRegistrationConverter.fromResponse(inputLegacyNewsstandRegistration, registrationResponse)
+      outputNewstandRegistration must beEqualTo(inputLegacyNewsstandRegistration)
     }
   }
 
