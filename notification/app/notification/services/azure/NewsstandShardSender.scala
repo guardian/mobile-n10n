fd072b86b26e1d9fa8519b0814837c0756470210
Alexandre Dufournet <alex.dufournet@theguardian.com>
2018-06-13 11:07:58 +0100

    Fix merge conflicts

--- notification/app/notification/services/azure/NewsstandShardSender.scala
+++ notification/app/notification/services/azure/NewsstandShardSender.scala
@@ -1,7 +1,7 @@
 package notification.services.azure
 
 import azure.NotificationHubClient
-import models.Notification
+import models.{Newsstand, Notification, Platform}
 import models.NotificationType.NewsstandShard
 import notification.services.Configuration
 import tracking.TopicSubscriptionsRepository
@@ -10,6 +10,8 @@ import scala.concurrent.ExecutionContext
 
 class NewsstandShardSender(hubClient: NotificationHubClient, configuration: Configuration, topicSubscriptionsRepository: TopicSubscriptionsRepository)
                           (implicit ec: ExecutionContext) extends NotificationsHubSender(hubClient, configuration, topicSubscriptionsRepository)(ec) {
+
+  override protected def platform: Platform = Newsstand
 
   override protected val converter = new APNSPushConverter(configuration)
 
