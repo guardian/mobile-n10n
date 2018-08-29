70f858ad2b5b9560cca70207b582496cc88eed70
Mark Richards <mark.richards@guardian.co.uk>
2018-08-29 18:58:52 +0100

    Logging added

--- eventconsumer/src/main/scala/com/gu/notifications/events/Lambda.scala
+++ eventconsumer/src/main/scala/com/gu/notifications/events/Lambda.scala
@@ -4,12 +4,14 @@ import java.io.{InputStream, OutputStream}
 
 import com.amazonaws.services.lambda.runtime.{Context, RequestStreamHandler}
 import com.amazonaws.util.IOUtils
+import org.apache.logging.log4j.LogManager
 import play.api.libs.json.Json
 
 object Lambda extends App {
   new Lambda("aws-mobile-event-logs-CODE")
 }
 class Lambda(eventConsumer: S3Event => Unit) extends RequestStreamHandler {
+  val logger = LogManager.getLogger(classOf[Lambda])
   def this(eventBucket:String) = this(e => new ProcessEvents(eventBucket))
   def this() = this(System.getenv("EventBucket"))
   override def handleRequest(input: InputStream, output: OutputStream, context: Context): Unit = {
@@ -19,6 +21,7 @@ class Lambda(eventConsumer: S3Event => Unit) extends RequestStreamHandler {
     finally {
       input.close()
     }
+    logger.info(inputString)
     S3Event.jf.reads(Json.parse(inputString)).foreach(eventConsumer)
   }
 
