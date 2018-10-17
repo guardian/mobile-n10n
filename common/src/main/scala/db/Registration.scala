2476eafa3d2738a0acae6917d2fa2a556fb7e10f
Thomas Bonnin <thomas.bonnin@gmail.com>
2018-10-17 10:33:20 +0100

    db layer: change lastModified type to LocalDateTime

--- common/src/main/scala/db/Registration.scala
+++ common/src/main/scala/db/Registration.scala
@@ -1,6 +1,7 @@
 package db
 
-import java.sql.Date
+import java.sql.Timestamp
+import java.time.{LocalDateTime, ZoneOffset}
 
 import doobie.util.meta.Meta
 import models.Platform
@@ -11,9 +12,15 @@ object Registration {
       s => Platform.fromString(s).getOrElse(throw doobie.util.invariant.InvalidEnum[Platform](s)),
       _.toString
     )
+
+  implicit val DateTimeMeta: Meta[LocalDateTime] =
+    Meta[Timestamp].xmap(
+      ts => ts.toLocalDateTime,
+      dt => Timestamp.valueOf(dt)
+    )
 }
 
-case class Registration(device: Device, topic: Topic, shard: Shard, lastModified: Option[Date] = None)
+case class Registration(device: Device, topic: Topic, shard: Shard, lastModified: Option[LocalDateTime] = None)
 case class Device(token: String, platform: Platform)
 case class Topic(name: String)
 case class Shard(id: Short)
