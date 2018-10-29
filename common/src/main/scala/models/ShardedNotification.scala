ee825f62a97618422b48f324b703a32a57ca4320
Alexandre Dufournet <alex.dufournet@theguardian.com>
2018-10-29 17:56:59 +0000

    add shard to the SQS payload

--- common/src/main/scala/models/ShardedNotification.scala
+++ common/src/main/scala/models/ShardedNotification.scala
@@ -1,10 +1,20 @@
 package models
 
+import play.api.libs.json.{Format, Json}
+
 case class ShardRange(start: Short, end: Short) {
   def range: Range = Range.inclusive(start, end)
+}
+
+object ShardRange {
+  implicit val shardRangeJF: Format[ShardRange] = Json.format[ShardRange]
 }
 
 case class ShardedNotification(
   notification: Notification,
   range: ShardRange
 )
+
+object ShardedNotification {
+  implicit val shardedNotificationJF: Format[ShardedNotification] = Json.format[ShardedNotification]
+}
