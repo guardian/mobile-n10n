295a3816940c902840a4d548f88ccaea68248c44
Alexandre Dufournet <alex.dufournet@theguardian.com>
2018-10-31 13:32:17 +0000

    Adding count fallback on the Fitlered notification sender

--- notification/app/notification/services/FilteredNotificationSender.scala
+++ notification/app/notification/services/FilteredNotificationSender.scala
@@ -1,16 +1,20 @@
 package notification.services
 import models._
 import notification.models.Push
-import notification.services.guardian.TopicRegistrationCounter
+import notification.services.guardian.{GuardianNotificationSender, TopicRegistrationCounter}
 import org.joda.time.DateTime
+import play.api.Logger
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.control.NonFatal
 
 class FilteredNotificationSender(
   notificationSender: NotificationSender,
   topicRegistrationCounter: TopicRegistrationCounter,
   invertCondition: Boolean
 )(implicit ec: ExecutionContext) extends NotificationSender {
+
+  private val logger: Logger = Logger.apply(classOf[FilteredNotificationSender])
 
   val ALLOWED_TOPICS: Set[TopicType] = Set(TopicTypes.Content, TopicTypes.TagContributor, TopicTypes.TagSeries)
 
@@ -30,8 +34,17 @@ class FilteredNotificationSender(
       }
     }
 
+    def defaultCountRecovery: PartialFunction[Throwable, PlatformCount] = {
+      case NonFatal(e) if push.notification.topic.exists(_.`type` == TopicTypes.Breaking) =>
+        logger.error(s"Unable to count registration for topics ${push.notification.topic}, falling back to default value", e)
+        PlatformCount(1500000, 750000, 750000, 0)
+      case NonFatal(e) =>
+        logger.error(s"Unable to count registration for topics ${push.notification.topic}, falling back to default value", e)
+        PlatformCount(1, 1, 1, 0)
+    }
+
     for {
-      count <- topicRegistrationCounter.count(push.notification.topic)
+      count <- topicRegistrationCounter.count(push.notification.topic).recover(defaultCountRecovery)
       result <- sendOrFilter(count, push.notification.topic)
     } yield result
   }
