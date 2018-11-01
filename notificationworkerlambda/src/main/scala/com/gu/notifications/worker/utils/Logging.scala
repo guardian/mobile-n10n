e09b6a41ff96bc30842e7a461a2d58ddd9a6e6c7
Thomas Bonnin <thomas.bonnin@gmail.com>
2018-11-01 10:11:37 +0000

    notificationworker: use toString when logging

--- notificationworkerlambda/src/main/scala/com/gu/notifications/worker/utils/Logging.scala
+++ notificationworkerlambda/src/main/scala/com/gu/notifications/worker/utils/Logging.scala
@@ -7,7 +7,7 @@ import org.slf4j.Logger
 trait Logging {
   def logger: Logger
 
-  private def log[A](prefix: String, logging: String => Unit): Pipe[IO, A, A] = _.evalMap{ a => IO.delay{ logging(s"$prefix: $a"); a}}
+  private def log[A](prefix: String, logging: String => Unit): Pipe[IO, A, A] = _.evalMap{ a => IO.delay{ logging(s"$prefix: ${a.toString}"); a}}
   def logInfo[A](prefix: String = ""): Pipe[IO, A, A] = log(prefix, logger.info)
   def logWarn[A](prefix: String = ""): Pipe[IO, A, A] = log(prefix, logger.warn)
   def logError[A](prefix: String = ""): Pipe[IO, A, A] = log(prefix, logger.error)
