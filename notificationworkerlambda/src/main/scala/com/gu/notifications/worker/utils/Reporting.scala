b3842b8dee9dacb1239999abc51fab4dc533a1ef
Alexandre Dufournet <alex.dufournet@theguardian.com>
2018-11-14 15:31:15 +0000

    Consider dry run as a success rather than a failure

--- notificationworkerlambda/src/main/scala/com/gu/notifications/worker/utils/Reporting.scala
+++ notificationworkerlambda/src/main/scala/com/gu/notifications/worker/utils/Reporting.scala
@@ -1,7 +1,6 @@
 package com.gu.notifications.worker.utils
 
 import cats.effect.IO
-import com.gu.notifications.worker.delivery.DeliveryException.DryRun
 import com.gu.notifications.worker.delivery.{DeliveryClient, DeliveryException, DeliverySuccess}
 import org.slf4j.Logger
 
@@ -10,7 +9,6 @@ object Reporting {
   def log[C <: DeliveryClient](prefix: String)(implicit logger: Logger): Either[DeliveryException, DeliverySuccess] => IO[Unit] = { resp =>
     IO.delay {
       resp match {
-        case Left(DryRun(_, _)) => () // no need to trace each individual token in the logs
         case Left(e) => logger.error(s"$prefix $e")
         case Right(_) => () // doing nothing when success
       }
