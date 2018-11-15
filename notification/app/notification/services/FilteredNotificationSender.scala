2dc503f3bfe7ba6ce5fa8088bdad832e3b0c92f7
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2018-11-15 10:31:20 +0000

    Revert "Revert "Bump the threshold high enough to deliver the morning briefing""

--- notification/app/notification/services/FilteredNotificationSender.scala
+++ notification/app/notification/services/FilteredNotificationSender.scala
@@ -22,11 +22,13 @@ class FilteredNotificationSender(
     TopicTypes.TagSeries,
     TopicTypes.TagBlog
   )
-  val maxRegistrationCount: Int = 10000
+  val maxRegistrationCount: Int = 300000
 
   override def sendNotification(push: Push): Future[SenderResult] = {
     def shouldSend(count: PlatformCount, topics: List[Topic]): Boolean = {
-      count.total < maxRegistrationCount && topics.forall(t => allowedTopicTypes.contains(t.`type`))
+      count.total < maxRegistrationCount &&
+        topics.forall(t => allowedTopicTypes.contains(t.`type`)) &&
+        push.avoidGuardianProvider.forall(_ == false)
     }
 
     def sendOrFilter(count: PlatformCount, topics: List[Topic]): Future[SenderResult] = {
