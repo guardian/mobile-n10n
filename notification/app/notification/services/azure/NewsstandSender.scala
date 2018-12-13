680d35c0c21ad13a5a5afcf761eec9870db8ec6e
Alexandre Dufournet <alex.dufournet@theguardian.com>
2018-12-13 15:13:02 +0000

    Cleanup newsstand

--- notification/app/notification/services/azure/NewsstandSender.scala
+++ notification/app/notification/services/azure/NewsstandSender.scala
@@ -3,37 +3,21 @@ package notification.services.azure
 import java.time.{Clock, Duration, Instant}
 import java.util.UUID
 
-import azure.{APNSRawPush, NotificationHubClient, Tags}
 import com.gu.notificationschedule.dynamo.{NotificationSchedulePersistenceAsync, NotificationsScheduleEntry}
-import models.{NewsstandShardConfig, NewsstandShardNotification, Topic}
-import models.TopicTypes.Newsstand
-import notification.models.ios.NewsstandNotification
+import models.{NewsstandShardConfig, NewsstandShardNotification}
 import play.api.libs.json.Json
 
-import scala.collection.immutable
 import scala.concurrent.{ExecutionContext, Future}
 
-
 class NewsstandSender(
-                       hubClient: NotificationHubClient,
-                       legacyNewsstandRegistrationConverterConfig:NewsstandShardConfig,
-                       notificationSchedulePersistence: NotificationSchedulePersistenceAsync,
-                       clock: Clock = Clock.systemUTC()
-                     )(implicit executionContext: ExecutionContext) {
+  legacyNewsstandRegistrationConverterConfig:NewsstandShardConfig,
+  notificationSchedulePersistence: NotificationSchedulePersistenceAsync,
+  clock: Clock = Clock.systemUTC()
+)(implicit executionContext: ExecutionContext) {
   val sevenDaysInSeconds = Duration.ofDays(7).getSeconds
-  def sendNotification(id: UUID) = {
-    val push = APNSRawPush(
-      body = NewsstandNotification(id).payload,
-      tags = Some(Tags.fromTopics(Set(Topic(Newsstand, "newsstand"))))
-    )
-    hubClient.sendNotification(push).flatMap(hubResult => {
-      scheduleShards(id).map(_ => hubResult)
-    })
-  }
-
-  private def scheduleShards (id: UUID): Future[immutable.IndexedSeq[Unit]] = {
+  def sendNotification(id: UUID): Future[Unit] = {
     val nowSeconds = Instant.now(clock).getEpochSecond
-    Future.sequence {
+    val result = Future.sequence {
       Range(0, legacyNewsstandRegistrationConverterConfig.shards.toInt).map(registeredShard => {
         val offset = registeredShard + 1
         val shardUuid = new UUID(id.getMostSignificantBits, id.getLeastSignificantBits + offset)
