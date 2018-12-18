f14df8ea74b963c531b5efc025f1b1530f5cf1a3
Mark Richards <mark.richards@guardian.co.uk>
2018-12-18 16:39:53 +0000

    Log cause on FailedException as error

--- notificationworkerlambda/src/main/scala/com/gu/notifications/worker/utils/Reporting.scala
+++ notificationworkerlambda/src/main/scala/com/gu/notifications/worker/utils/Reporting.scala
@@ -1,7 +1,7 @@
 package com.gu.notifications.worker.utils
 
 import cats.effect.IO
-import com.gu.notifications.worker.delivery.DeliveryException.InvalidToken
+import com.gu.notifications.worker.delivery.DeliveryException.{FailedRequest, InvalidToken}
 import com.gu.notifications.worker.delivery.{DeliveryClient, DeliveryException, DeliverySuccess}
 import org.slf4j.Logger
 
@@ -11,7 +11,8 @@ object Reporting {
     IO.delay {
       resp match {
         case Left(e: InvalidToken) => logger.warn(s"$prefix $e")
-        case Left(e) => logger.error(s"$prefix $e", e.getCause)
+        case Left(e: FailedRequest) => logger.warn(s"$prefix $e", e.cause)
+        case Left(e) => logger.error(prefix ,e)
         case Right(_) => () // doing nothing when success
       }
     }
