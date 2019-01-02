ec08a920bfbb8bb13278b6f80d6cb22f69b61a18
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2019-01-02 11:49:18 +0000

    set football to ttl back to an hour after testing with 10 mins

--- notificationworkerlambda/src/main/scala/com/gu/notifications/worker/delivery/utils/TimeToLive.scala
+++ notificationworkerlambda/src/main/scala/com/gu/notifications/worker/delivery/utils/TimeToLive.scala
@@ -3,5 +3,5 @@ package com.gu.notifications.worker.delivery.utils
 object TimeToLive {
   val DefaulTtl: Long = 86400000L //24 hours
   val BreakingNewsTtl: Long = 3600000L //12 hours
-  val FootballMatchStatusTtl: Long = 600000L //10 minutes
+  val FootballMatchStatusTtl: Long = 3600000L //1 hour
 }
