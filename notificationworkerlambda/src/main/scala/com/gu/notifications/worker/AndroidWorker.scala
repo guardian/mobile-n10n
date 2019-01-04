d48b66e499d42c5f37d61c9b36addfe9fdd72d1a
Alexandre Dufournet <alex.dufournet@theguardian.com>
2019-01-04 10:28:02 +0000

    split cleaning client into trait and implementation

--- notificationworkerlambda/src/main/scala/com/gu/notifications/worker/AndroidWorker.scala
+++ notificationworkerlambda/src/main/scala/com/gu/notifications/worker/AndroidWorker.scala
@@ -5,14 +5,14 @@ import com.gu.notifications.worker.delivery.fcm.{Fcm, FcmClient}
 import db.{DatabaseConfig, RegistrationService}
 import doobie.util.transactor.Transactor
 import _root_.models.Android
-import com.gu.notifications.worker.cleaning.CleaningClient
+import com.gu.notifications.worker.cleaning.CleaningClientImpl
 
 class AndroidWorker extends WorkerRequestHandler[FcmClient] {
   val platform = Android
   val config: FcmWorkerConfiguration = Configuration.fetchFirebase()
   val transactor: Transactor[IO] = DatabaseConfig.transactor[IO](config.jdbcConfig)
   val registrationService = RegistrationService(transactor)
-  val cleaningClient = new CleaningClient(config.sqsUrl)
+  val cleaningClient = new CleaningClientImpl(config.sqsUrl)
 
   override val deliveryService: IO[Fcm[IO]] =
     FcmClient(config.fcmConfig).fold(e => IO.raiseError(e), c => IO.delay(new Fcm(registrationService, c)))
--- notificationworkerlambda/src/main/scala/com/gu/notifications/worker/IOSWorker.scala
+++ notificationworkerlambda/src/main/scala/com/gu/notifications/worker/IOSWorker.scala
@@ -5,14 +5,14 @@ import com.gu.notifications.worker.delivery.apns.{Apns, ApnsClient}
 import db.{DatabaseConfig, RegistrationService}
 import doobie.util.transactor.Transactor
 import _root_.models.iOS
-import com.gu.notifications.worker.cleaning.CleaningClient
+import com.gu.notifications.worker.cleaning.CleaningClientImpl
 
 class IOSWorker extends WorkerRequestHandler[ApnsClient] {
   val platform = iOS
   val config: ApnsWorkerConfiguration = Configuration.fetchApns()
   val transactor: Transactor[IO] = DatabaseConfig.transactor[IO](config.jdbcConfig)
   val registrationService = RegistrationService(transactor)
-  val cleaningClient = new CleaningClient(config.sqsUrl)
+  val cleaningClient = new CleaningClientImpl(config.sqsUrl)
 
   override val deliveryService: IO[Apns[IO]] =
     ApnsClient(config.apnsConfig).fold(e => IO.raiseError(e), c => IO.delay(new Apns(registrationService, c)))
