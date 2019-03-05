2f3f9ce52bdc3fc21d0ea32367bac59b4a80cb76
Mark Richards <mark.richards@guardian.co.uk>
2019-03-05 17:14:25 +0000

    MSS-735: Prepare for restricted newsstand api keys

--- notification/app/notification/services/Configuration.scala
+++ notification/app/notification/services/Configuration.scala
@@ -11,6 +11,7 @@ class Configuration(conf: PlayConfig) {
   lazy val frontendNewsAlertApiKey: String = conf.get[String]("notifications.frontendNewsAlert.apiKey")
   lazy val dynamoReportsTableName: String = conf.get[String]("db.dynamo.reports.table-name")
   lazy val dynamoScheduleTableName: String = conf.get[String]("db.dynamo.schedule.table-name")
+  lazy val newsstandRestrictedApiKeys: Set[String] = conf.get[Seq[String]]("notifications.api.newsstandRestrictedKeys").toSet
 
   lazy val newsstandShards: Int = conf.get[Int]("newsstand.shards")
 }
