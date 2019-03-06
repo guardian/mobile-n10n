afb54c7acc3931da0d2081231e4b326cee4ef8ec
Mark Richards <mark.richards@guardian.co.uk>
2019-03-06 13:43:13 +0000

    Reference to elections key should be newsstand

--- notification/app/notification/authentication/NotificationAuthAction.scala
+++ notification/app/notification/authentication/NotificationAuthAction.scala
@@ -9,7 +9,7 @@ import play.api.mvc.ControllerComponents
 
 class NotificationAuthAction(configuration: Configuration,  controllerComponents: ControllerComponents) extends AuthAction(controllerComponents) {
 
-  override def validApiKey(apiKey: String): Boolean = configuration.apiKeys.contains(apiKey) || configuration.electionRestrictedApiKeys.contains(apiKey)
+  override def validApiKey(apiKey: String): Boolean = configuration.apiKeys.contains(apiKey) || configuration.newsstandRestrictedApiKeys.contains(apiKey)
 
   override def isPermittedTopic(apiKey: String): Topic => Boolean = {
     if(configuration.newsstandRestrictedApiKeys.contains(apiKey)) {
