c7488e7d2d8a62e8acb9ac15e80b851bd7ba57c4
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2019-03-25 17:41:37 +0000

    change before rebasing

--- api-client/src/main/scala/com/gu/mobile.notifications.client/NextGenApiClient.scala
+++ api-client/src/main/scala/com/gu/mobile.notifications.client/NextGenApiClient.scala
@@ -23,13 +23,44 @@ protected class NextGenApiClient(
   private val url = s"$host/push/topic"
 
   override def send(notificationPayload: NotificationPayload)(implicit ec: ExecutionContext): Future[Either[ApiClientError, Unit]] = {
-    val json = Json.stringify(Json.toJson(notificationPayload))
-    postJson(url, json) map {
-      case error: HttpError => Left(ApiHttpError(error.status, Some(error.body)))
-      case HttpOk(201, body) => validateFormat[NextGenResponse](body)
-      case HttpOk(code, body) => Left(UnexpectedApiResponseError(s"Server returned status code $code and body:$body"))
-    } recover {
-      case e: Exception => Left(HttpProviderError(e))
+
+    def doSend(payload: NotificationPayload): Future[Either[ApiClientError, Unit]] = {
+      val json = Json.stringify(Json.toJson(payload))
+      postJson(url, json) map {
+        case error: HttpError => Left(ApiHttpError(error.status, Some(error.body)))
+        case HttpOk(201, body) => validateFormat[NextGenResponse](body)
+        case HttpOk(code, body) => Left(UnexpectedApiResponseError(s"Server returned status code $code and body:$body"))
+      } recover {
+        case e: Exception => Left(HttpProviderError(e))
+      }
+    }
+
+    def doSendOncePerTopic(breakingNewsPayload: BreakingNewsPayload): Future[Either[ApiClientError, Unit]] = {
+      val editions = List("uk", "international", "us", "au")
+      val orderedTopicsToSend = editions.flatMap {
+        ed => breakingNewsPayload.topic.find(_.name == s"breaking/${ed}")
+      }
+      val orderedPayloads = orderedTopicsToSend.map { topic => breakingNewsPayload.copy(topic = List(topic), id =UUID.randomUUID()) }
+
+        val responsesFuture = orderedPayloads.foldLeft(Future.successful(Seq.empty[Either[ApiClientError, Unit]])) {
+        (futureResults, payload) => futureResults.flatMap{ results => {
+          //doSend(payload).transform()
+          doSend(payload).map(result => results :+ result) }
+        }
+      }
+
+      responsesFuture.map { responses =>
+        responses.collect { case Left(error) => error } match {
+          case Nil => Right(())
+          case onlyOneError :: Nil => Left(onlyOneError)
+         ` case errors: List[ApiClientError] => Left(UnexpectedApiResponseError(errors.map(_.description).mkString(", ")))
+        }
+      }
+    }
+
+    notificationPayload match {
+      case breakingNews: BreakingNewsPayload if breakingNews.topic.size > 1 => doSendOncePerTopic(breakingNews)
+      case _ => doSend(notificationPayload)
     }
   }
 
