228199863549ff640da2b573e2d34b1180b1b375
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2019-07-04 18:34:36 +0100

    Fix the report service such that it can parse the event aggregation with both attribute names (platformCounts and platform)

--- commoneventconsumer/src/main/scala/com/gu/notifications/events/model/EventAggregation.scala
+++ commoneventconsumer/src/main/scala/com/gu/notifications/events/model/EventAggregation.scala
@@ -1,11 +1,21 @@
 package com.gu.notifications.events.model
 
-import play.api.libs.json.{Json, Writes}
+import play.api.libs.json._
 
 
 case class EventAggregation(platformCounts: PlatformCount)
 
 object EventAggregation {
-  implicit val jreads = Json.reads[EventAggregation]
+  private val defaultJsonReads = Json.reads[EventAggregation]
+
+  implicit val jreads = new Reads[EventAggregation] {
+    override def reads(json: JsValue): JsResult[EventAggregation] = json match {
+      case JsObject(fields) =>
+        val platformCount = fields.get("platform").orElse(fields.get("platformCounts"))
+        val newFields = platformCount.map(pc => Map("platformCounts" -> pc)).getOrElse(Map.empty)
+        defaultJsonReads.reads(JsObject(newFields))
+      case _ => JsError("Expected a JsObject to parse EventAggregation")
+    }
+  }
   implicit val jwrites: Writes[EventAggregation] = Json.writes[EventAggregation]
 }
