3d87d9673a3d73bd16e3eb4572d83d939cd5ed6c
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2019-07-12 12:28:12 +0100

    Drop as much logic as possible to only keep the models

--- api-models/src/main/scala/com/gu/mobile.notifications.client/lib/JsonFormatsHelper.scala
+++ api-models/src/main/scala/com/gu/mobile.notifications.client/lib/JsonFormatsHelper.scala
@@ -32,35 +32,6 @@ object JsonFormatsHelper {
     def withAdditionalStringFields(fields: Map[String, String]): Writes[A] = withAdditionalFields(fields.mapValues(JsString))
   }
 
-  implicit class RichReads[A](innerReads: Reads[A]) {
-    def withTypeString(typ: String): Reads[A] = new Reads[A] {
-      def reads(json: JsValue): JsResult[A] = json match {
-        case obj: JsObject if obj.value.get("type") == Some(JsString(typ)) => innerReads.reads(obj - "type")
-        case _: JsObject => JsError(s"Not of type $typ")
-        case _ => JsError("Not an object")
-      }
-    }
-  }
-
-  implicit class RichFormat[A](format: Format[A]) {
-    def withTypeString(typ: String): Format[A] = new Format[A] {
-      private val richWrites = new RichWrites[A](format).withTypeString(typ)
-      private val richReads = new RichReads[A](format).withTypeString(typ)
-      override def writes(o: A): JsValue = richWrites.writes(o)
-      override def reads(json: JsValue): JsResult[A] = richReads.reads(json)
-    }
-  }
-
-  class TypedSubclassReads[A](val subClassReads: List[Reads[_ <: A]]) extends Reads[A] {
-    def reads(json: JsValue): JsResult[A] = {
-      def iter(reads: List[Reads[_ <: A]]): JsResult[A] = reads match {
-        case x :: xs => x.reads(json) orElse iter(xs)
-        case Nil => JsError("No match for typed subclasses")
-      }
-
-      iter(subClassReads)
-    }
-  }
 
   implicit val urlFormat = new Format[URI] {
     override def writes(o: URI): JsValue = JsString(o.toString)
