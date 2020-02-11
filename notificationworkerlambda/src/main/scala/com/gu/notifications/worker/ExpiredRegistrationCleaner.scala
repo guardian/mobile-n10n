c2a2a044a79e7d9e8f7d9060a25a0ff5d67a2843
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2020-02-11 17:13:29 +0000

    Create structure to run lambda locally

--- notificationworkerlambda/src/main/scala/com/gu/notifications/worker/ExpiredRegistrationCleaner.scala
+++ notificationworkerlambda/src/main/scala/com/gu/notifications/worker/ExpiredRegistrationCleaner.scala
@@ -3,11 +3,24 @@ package com.gu.notifications.worker
 import com.gu.notifications.worker.utils.Logging
 import org.slf4j.{Logger, LoggerFactory}
 
-class ExpiredRegistrationCleanerLambda extends Logging {
-
+class ExpiredRegistrationCleaner {
   def logger: Logger = LoggerFactory.getLogger(this.getClass)
 
-  def handleRequest(): Unit = {
+  def clean(): Unit = {
     logger.info("Hello")
+  }
+}
+
+class ExpiredRegistrationCleanerLambda {
+  def handleRequest(): Unit = {
+    val cleaner = new ExpiredRegistrationCleaner
+    cleaner.clean()
+  }
+}
+
+object ExpiredRegistrationCleanerLocalRun {
+  def main(args: Array[String]): Unit = {
+    val cleaner = new ExpiredRegistrationCleaner
+    cleaner.clean()
   }
 }
