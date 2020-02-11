6b0e48f8e146f0c6a060d299876b2fedd75e90f9
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2020-02-11 17:23:56 +0000

    Connect to the database

--- notificationworkerlambda/src/main/scala/com/gu/notifications/worker/ExpiredRegistrationCleaner.scala
+++ notificationworkerlambda/src/main/scala/com/gu/notifications/worker/ExpiredRegistrationCleaner.scala
@@ -1,7 +1,13 @@
 package com.gu.notifications.worker
 
-import com.gu.notifications.worker.utils.Logging
+import cats.effect.{ContextShift, IO}
+import com.amazonaws.auth.AWSCredentialsProviderChain
+import com.gu.notifications.worker.utils.Aws
+import db.DatabaseConfig
+import doobie.util.transactor.Transactor
 import org.slf4j.{Logger, LoggerFactory}
+
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
 
 class ExpiredRegistrationCleaner {
   def logger: Logger = LoggerFactory.getLogger(this.getClass)
@@ -12,6 +18,16 @@ class ExpiredRegistrationCleaner {
 }
 
 class ExpiredRegistrationCleanerLambda {
+  def env = Env()
+
+  lazy val credentials: AWSCredentialsProviderChain = Aws.credentialsProvider
+
+  implicit val ec: ExecutionContextExecutor = ExecutionContext.global
+  implicit val ioContextShift: ContextShift[IO] = IO.contextShift(ec)
+
+  val config: CleanerConfiguration = Configuration.fetchCleaner()
+  val transactor: Transactor[IO] = DatabaseConfig.transactor[IO](config.jdbcConfig)
+
   def handleRequest(): Unit = {
     val cleaner = new ExpiredRegistrationCleaner
     cleaner.clean()
@@ -20,7 +36,7 @@ class ExpiredRegistrationCleanerLambda {
 
 object ExpiredRegistrationCleanerLocalRun {
   def main(args: Array[String]): Unit = {
-    val cleaner = new ExpiredRegistrationCleaner
-    cleaner.clean()
+    val cleaner = new ExpiredRegistrationCleanerLambda
+    cleaner.handleRequest()
   }
 }
