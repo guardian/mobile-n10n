1ef1c7b86ab0cfcc550fad9cd85380d9f7551f6c
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2020-04-30 11:21:48 +0100

    Fix unit tests

--- registration/test/registration/auditor/FootballMatchAuditorSpec.scala
+++ registration/test/registration/auditor/FootballMatchAuditorSpec.scala
@@ -2,11 +2,10 @@ package registration.auditor
 
 import models.{Topic, TopicTypes}
 import org.joda.time.DateTime
-import org.mockito.Matchers.{eq => argEq}
 import org.specs2.concurrent.ExecutionEnv
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
-import pa.{MatchDay, PaClient}
+import pa.{Http, MatchDay, PaClient, Response}
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
@@ -14,8 +13,7 @@ import scala.concurrent.{ExecutionContext, Future}
 class FootballMatchAuditorSpec(implicit ev: ExecutionEnv) extends Specification with Mockito {
   "Football match client" should {
     "query PA and return matches that have ended" in {
-      val paClient = mock[PaClient]
-      val matchDay = MatchDay(
+      val defaultMatchDay = MatchDay(
         id =  "match-id-1",
         date =  DateTime.now,
         competition = None,
@@ -35,9 +33,17 @@ class FootballMatchAuditorSpec(implicit ev: ExecutionEnv) extends Specification 
         venue = None,
         comments = None
       )
-      paClient.matchInfo(argEq("match-id-1"))(any[ExecutionContext]) returns Future.successful(matchDay.copy(id = "match-id-1", matchStatus = "FT"))
-      paClient.matchInfo(argEq("match-id-2"))(any[ExecutionContext]) returns Future.successful(matchDay.copy(id = "match-id-2", matchStatus = "HT"))
-      paClient.matchInfo(argEq("match-id-3"))(any[ExecutionContext]) returns Future.successful(matchDay.copy(id = "match-id-3", matchStatus = "Result"))
+
+      val paClient = new PaClient with Http {
+        override def apiKey: String = ""
+        override def GET(url: String): Future[Response] = ???
+
+        override def matchInfo(id: String)(implicit context: ExecutionContext): Future[MatchDay] = id match {
+          case "match-id-1" => Future.successful(defaultMatchDay.copy(id = "match-id-1", matchStatus = "FT"))
+          case "match-id-2" => Future.successful(defaultMatchDay.copy(id = "match-id-2", matchStatus = "HT"))
+          case "match-id-3" => Future.successful(defaultMatchDay.copy(id = "match-id-3", matchStatus = "Result"))
+        }
+      }
 
       val topics = Set(
         Topic(TopicTypes.Breaking, "test-1"),
@@ -54,7 +60,13 @@ class FootballMatchAuditorSpec(implicit ev: ExecutionEnv) extends Specification 
     }
 
     "do not query PA if there are no football matches in topic list" in {
-      val paClient = mock[PaClient]
+      val paClient = new PaClient with Http {
+        override def apiKey: String = ""
+        override def GET(url: String): Future[Response] = ???
+
+        override def matchInfo(id: String)(implicit context: ExecutionContext): Future[MatchDay] = throw new Exception("This should not be called")
+      }
+
       val topics = Set(
         Topic(TopicTypes.Breaking, "test-1"),
         Topic(TopicTypes.Content, "test-2"),
@@ -64,7 +76,6 @@ class FootballMatchAuditorSpec(implicit ev: ExecutionEnv) extends Specification 
       val auditor = FootballMatchAuditor(paClient)
       val filteredTopics = auditor.expiredTopics(topics)
       filteredTopics must beEqualTo(Set.empty).awaitFor(5 seconds)
-      there were no(paClient).matchInfo(any[String])(any[ExecutionContext])
     }
   }
 }
