6631a546706bf34e8dfc6aba92a314935d43c1b6
Jacob Winch <jacob.winch@guardian.co.uk>
2020-10-13 12:07:59 +0100

    Wire up error handler

--- registration/app/registration/ErrorHandler.scala
+++ registration/app/registration/ErrorHandler.scala
@@ -1,9 +1,11 @@
+package registration
+
 import javax.inject._
 import org.slf4j.{Logger, LoggerFactory}
-import play.api.http.DefaultHttpErrorHandler
 import play.api._
-import play.api.mvc._
+import play.api.http.DefaultHttpErrorHandler
 import play.api.mvc.Results._
+import play.api.mvc._
 import play.api.routing.Router
 
 import scala.concurrent._
