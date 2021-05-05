98ee7e4663eed3cd6b4bda31796fa028849ed83e
Francesca Hammond <francesca.hammond@guardian.co.uk>
2021-05-05 14:40:14 +0100

    Update credentials to use v2

--- common/src/main/scala/utils/CustomApplicationLoader.scala
+++ common/src/main/scala/utils/CustomApplicationLoader.scala
@@ -6,6 +6,11 @@ import com.gu.conf.{ConfigurationLoader, SSMConfigurationLocation}
 import com.gu.{AppIdentity, AwsIdentity}
 import play.api.ApplicationLoader.Context
 import play.api._
+import software.amazon.awssdk.auth.credentials.{
+  AwsCredentialsProviderChain => AwsCredentialsProviderChainV2,
+  ProfileCredentialsProvider => ProfileCredentialsProviderV2,
+  DefaultCredentialsProvider => DefaultCredentialsProviderV2
+}
 
 abstract class CustomApplicationLoader extends ApplicationLoader {
   def buildComponents(identity: AppIdentity, context: Context): BuiltInComponents
@@ -15,10 +20,15 @@ abstract class CustomApplicationLoader extends ApplicationLoader {
     InstanceProfileCredentialsProvider.getInstance
   )
 
+  lazy val credentialsv2 = AwsCredentialsProviderChainV2.of(
+    ProfileCredentialsProviderV2.builder.profileName("mobile").build,
+    DefaultCredentialsProviderV2.create
+  )
+
   override def load(context: Context): Application = {
     LoggerConfigurator(context.environment.classLoader) foreach { _.configure(context.environment) }
     val identity = AppIdentity.whoAmI("notifications")
-    val config = ConfigurationLoader.load(identity, credentials) {
+    val config = ConfigurationLoader.load(identity, credentialsv2) {
       case AwsIdentity(app, stack, stage, _) => SSMConfigurationLocation(s"/notifications/$stage/$stack")
     }
     val loadedConfig = Configuration(config)
