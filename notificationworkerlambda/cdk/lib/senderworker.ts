75a9b7b908f113150d854380451d43ffe7f21b83
Paul Roberts <paul.roberts@theguardian.com>
2021-12-15 19:47:32 +0000

    first alarm action

--- notificationworkerlambda/cdk/lib/senderworker.ts
+++ notificationworkerlambda/cdk/lib/senderworker.ts
@@ -1,8 +1,11 @@
 import * as cdk from '@aws-cdk/core'
 import * as ecr from '@aws-cdk/aws-ecr'
 import * as sqs from '@aws-cdk/aws-sqs'
+import * as sns from '@aws-cdk/aws-sns'
 import * as iam from '@aws-cdk/aws-iam'
 import * as lambda from '@aws-cdk/aws-lambda'
+import * as cloudwatch from '@aws-cdk/aws-cloudwatch'
+import {SnsAction} from '@aws-cdk/aws-cloudwatch-actions'
 import { GuStack } from "@guardian/cdk/lib/constructs/core"
 import type { App } from "@aws-cdk/core"
 import type { GuStackProps } from "@guardian/cdk/lib/constructs/core"
@@ -10,7 +13,9 @@ import type { GuStackProps } from "@guardian/cdk/lib/constructs/core"
 type SenderWorkerOpts = {
   handler: string,
   imageRepo: ecr.IRepository,
-  buildId: string
+  buildId: string,
+  reservedConcurrency: number,
+  alarmTopic: sns.ITopic
 }
 
 class SenderWorker extends cdk.Construct {
@@ -18,6 +23,8 @@ class SenderWorker extends cdk.Construct {
     super(scope, id)
 
     cdk.Tags.of(this).add("App", id)
+
+    const snsTopicAction = new SnsAction(opts.alarmTopic)
 
     const senderDlq = new sqs.Queue(this, 'SenderDlq')
     const senderSqs = new sqs.Queue(this, 'SenderSqs', {
@@ -99,8 +106,39 @@ class SenderWorker extends cdk.Construct {
 
     const senderLambdaCtr = new lambda.DockerImageFunction(this, 'SenderLambdaCtr', {
       functionName: `${scope.stack}-${id}-sender-ctr-${scope.stage}`,
-      code: codeImage
+      code: codeImage,
+      environment: {
+        Stage: scope.stage,
+        Stack: scope.stack,
+        App: id
+      },
+      memorySize: 3008,
+      description: `sends notifications for ${id}`,
+      role: executionRole,
+      timeout: cdk.Duration.seconds(90),
+      reservedConcurrentExecutions: opts.reservedConcurrency
     })
+
+    const senderSqsEventSourceMapping = new lambda.EventSourceMapping(this, "SenderSqsEventSourceMapping", {
+      batchSize: 1,
+      enabled: true,
+      eventSourceArn: senderSqs.queueArn,
+      target: senderLambdaCtr
+    })
+    senderSqsEventSourceMapping.node.addDependency(senderSqs)
+    senderSqsEventSourceMapping.node.addDependency(senderLambdaCtr)
+
+    const senderThrottleAlarm = new cloudwatch.Alarm(this, 'SenderErrorAlarm', {
+      alarmDescription: `Triggers if the ${id} sender lambda is throttled in ${scope.stage}.`,
+      comparisonOperator: cloudwatch.ComparisonOperator.GREATER_THAN_THRESHOLD,
+      evaluationPeriods: 1,
+      threshold: 0,
+      metric: senderLambdaCtr.metricThrottles({period: cdk.Duration.seconds(360)}),
+      statistic: "Sum",
+      treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING
+    })
+    senderThrottleAlarm.addAlarmAction(snsTopicAction)
+    senderThrottleAlarm.addOkAction(snsTopicAction)
   }
 }
 
@@ -108,9 +146,19 @@ export class SenderWorkerStack extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props)
 
+    const reservedConcurrencyParam = new cdk.CfnParameter(this, "ReservedConcurrency", {
+      type: "Number",
+      description: "How many concurrent execution to provision the lamdba with"
+    });
+
     const buildIdParam = new cdk.CfnParameter(this, "BuildId", {
       type: "String",
       description: "build id from teamcity, the image should be tagged with this"
+    });
+
+    const alarmTopicArnParam = new cdk.CfnParameter(this, "AlarmTopicArn", {
+      type: "String",
+      description: "The ARN of the SNS topic to send all the cloudwatch alarms to"
     });
 
     const notificationEcrRepo =
@@ -119,15 +167,19 @@ export class SenderWorkerStack extends GuStack {
         repositoryName: cdk.Fn.importValue("NotificationLambdaRepositoryName")
       })
 
+    let sharedOpts = {
+      imageRepo: notificationEcrRepo,
+      buildId: buildIdParam.valueAsString,
+      reservedConcurrency: reservedConcurrencyParam.valueAsNumber,
+      alarmTopic: sns.Topic.fromTopicArn(this, 'AlarmTopic', alarmTopicArnParam.valueAsString)
+    }
+
     new SenderWorker(this, "ios-worker", {
       handler: "com.gu.notifications.worker.AndroidSender::handleChunkTokens",
-      imageRepo: notificationEcrRepo,
-      buildId: buildIdParam.valueAsString
+      ...sharedOpts
     })
 
     // new SenderWorker(this, "android-worker")
 
   }
-
-
 }
