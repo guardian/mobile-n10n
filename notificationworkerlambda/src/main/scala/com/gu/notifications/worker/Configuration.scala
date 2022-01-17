8ba4d95899dc35e66eeb587a2e040c237329e075
Paul Roberts <paul.roberts@theguardian.com>
2022-01-17 16:31:37 +0000

    do not talk to cdk stack initially
    but do keep both of the arn lists in the policy

--- notificationworkerlambda/src/main/scala/com/gu/notifications/worker/Configuration.scala
+++ notificationworkerlambda/src/main/scala/com/gu/notifications/worker/Configuration.scala
@@ -68,11 +68,18 @@ object Configuration {
     val config = fetchConfiguration("harvester")
     HarvesterConfiguration(
       jdbcConfig = jdbcConfig(config),
-      iosLiveSqsUrl = config.getString("iosLiveSqsCdkUrl"),
+      // currently this will ignore the CDK stack. To change to CDK, we should
+      // comment the next 5 lines, and uncomment the 5 lines following them.
+      iosLiveSqsUrl = config.getString("iosLiveSqsUrl"),
       iosEditionSqsUrl = config.getString("iosEditionSqsUrl"),
-      androidLiveSqsUrl = config.getString("androidLiveSqsCdkUrl"),
+      androidLiveSqsUrl = config.getString("androidLiveSqsUrl"),
       androidEditionSqsUrl = config.getString("androidEditionSqsUrl"),
       androidBetaSqsUrl = config.getString("androidBetaSqsUrl")
+      // iosLiveSqsUrl = config.getString("iosLiveSqsCdkUrl"),
+      // iosEditionSqsUrl = config.getString("iosEditionSqsUrl"),
+      // androidLiveSqsUrl = config.getString("androidLiveSqsCdkUrl"),
+      // androidEditionSqsUrl = config.getString("androidEditionSqsUrl"),
+      // androidBetaSqsUrl = config.getString("androidBetaSqsUrl")
     )
   }
 
