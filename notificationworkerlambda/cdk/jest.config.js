6375366fd97afbdad8d39216e22e608d6d8dcebb
Francesca Hammond <francesca.hammond@guardian.co.uk>
2022-03-31 14:50:18 +0100

    Add snapshot and testing tools for cdk

--- notificationworkerlambda/cdk/jest.config.js
+++ notificationworkerlambda/cdk/jest.config.js
@@ -1,8 +1,5 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  }
 };
