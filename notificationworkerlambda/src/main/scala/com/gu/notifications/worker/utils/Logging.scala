9cb0eeca3d376f15273b5e3edf615c1025175c3d
David Lawes <david.lawes@guardian.co.uk>
2022-06-30 12:33:18 +0100

    Lambda log as a js object

--- notificationworkerlambda/src/main/scala/com/gu/notifications/worker/utils/Logging.scala
+++ notificationworkerlambda/src/main/scala/com/gu/notifications/worker/utils/Logging.scala
@@ -4,17 +4,17 @@ import cats.effect.IO
 import org.slf4j.{Logger, Marker}
 
 import models.LoggingField
-import models.NotificationLogging.customLogstashFields
+import models.NotificationLogging.customLogsAsJsString
 
 trait Logging {
   def logger: Logger
 
   private def log[A](prefix: String, logging: String => Unit): A => IO[Unit] = a => IO.delay(logging(s"$prefix: ${a.toString}"))
   private def logWithCustomFields[A](prefix: String, marker: Marker, logging: (Marker, String) => Unit): A => IO[Unit] =
-    a => IO.delay(logging(marker, s"$prefix: ${a.toString} ${marker.toString}"))
+    a => IO.delay(logging(marker, s"$prefix: ${a.toString}"))
   def logInfo[A](prefix: String = ""): A => IO[Unit] = log(prefix, logger.info)
   def logWarn[A](prefix: String = ""): A => IO[Unit] = log(prefix, logger.warn)
   def logError[A](prefix: String = ""): A => IO[Unit] = log(prefix, logger.error)
   def logInfoWithCustomFields[A](prefix: String = "", customFields: List[LoggingField]): A => IO[Unit] =
-    logWithCustomFields(prefix, customLogstashFields(customFields), logger.info)
+    log(customLogsAsJsString(prefix, customFields), logger.info)
 }
