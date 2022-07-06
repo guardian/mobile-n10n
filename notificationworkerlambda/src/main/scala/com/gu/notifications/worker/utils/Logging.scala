a1ce1be96aa70c1ed182b186532230c8bb7c12ec
David Lawes <david.lawes@guardian.co.uk>
2022-07-06 11:23:55 +0100

    try appending keys using powertools logging utils

--- notificationworkerlambda/src/main/scala/com/gu/notifications/worker/utils/Logging.scala
+++ notificationworkerlambda/src/main/scala/com/gu/notifications/worker/utils/Logging.scala
@@ -2,6 +2,7 @@ package com.gu.notifications.worker.utils
 
 import cats.effect.IO
 import org.slf4j.{Logger, Marker}
+import software.amazon.lambda.powertools.logging.LoggingUtils
 
 import models.LoggingField
 import models.NotificationLogging.customLogstashFields
@@ -11,7 +12,10 @@ trait Logging {
 
   private def log[A](prefix: String, logging: String => Unit): A => IO[Unit] = a => IO.delay(logging(s"$prefix: ${a.toString}"))
   private def logWithCustomFields[A](prefix: String, marker: Marker, logging: (Marker, String) => Unit): A => IO[Unit] =
-    a => IO.delay(logging(marker, s"$prefix: ${a.toString}"))
+    a => IO.delay({
+      LoggingUtils.appendKey("notificationId", "test")
+      logging(marker, s"$prefix: ${a.toString}")
+    })
   def logInfo[A](prefix: String = ""): A => IO[Unit] = log(prefix, logger.info)
   def logWarn[A](prefix: String = ""): A => IO[Unit] = log(prefix, logger.warn)
   def logError[A](prefix: String = ""): A => IO[Unit] = log(prefix, logger.error)
