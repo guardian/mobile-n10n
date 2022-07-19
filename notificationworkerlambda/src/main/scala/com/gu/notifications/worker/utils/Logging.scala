f235101ed0d872b191a9e9101fc896d7a17db75a
David Lawes <45561419+DavidLawes@users.noreply.github.com>
2022-07-19 09:34:00 +0100

    Increase harvester logging (#685)

--- notificationworkerlambda/src/main/scala/com/gu/notifications/worker/utils/Logging.scala
+++ notificationworkerlambda/src/main/scala/com/gu/notifications/worker/utils/Logging.scala
@@ -1,10 +1,16 @@
 package com.gu.notifications.worker.utils
 
 import cats.effect.IO
+import net.logstash.logback.marker.LogstashMarker
+import net.logstash.logback.marker.Markers.appendEntries
 import org.slf4j.Logger
+
+import scala.jdk.CollectionConverters.MapHasAsJava
 
 trait Logging {
   def logger: Logger
+
+  implicit def mapToContext(c: Map[String, _]): LogstashMarker = appendEntries(c.asJava)
 
   private def log[A](prefix: String, logging: String => Unit): A => IO[Unit] = a => IO.delay(logging(s"$prefix: ${a.toString}"))
   def logInfo[A](prefix: String = ""): A => IO[Unit] = log(prefix, logger.info)
