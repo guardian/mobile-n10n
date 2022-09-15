916660a70568b3c8c512574837984161aaa17fce
Wai Sing Yiu <wai.sing.yiu@guardian.co.uk>
2022-09-15 16:24:51 +0100

    Change to 2000 tokens per message

--- notificationworkerlambda/src/main/scala/com/gu/notifications/worker/Harvester.scala
+++ notificationworkerlambda/src/main/scala/com/gu/notifications/worker/Harvester.scala
@@ -59,7 +59,7 @@ trait HarvesterRequestHandler extends Logging {
         .collect {
           case (targetSqs, harvestedToken) if targetSqs == workerSqs => harvestedToken.token
         }
-        .chunkN(1000)
+        .chunkN(2000)
         .map(chunk => ChunkedTokens(shardedNotification.notification, chunk.toList, shardedNotification.range))
         .map(deliveryService.sending)
         .parJoin(maxConcurrency)
