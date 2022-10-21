1fbb92f4f7366ce510e3fe6e65b51526ec25a18d
David Lawes <david.lawes@guardian.co.uk>
2022-10-21 10:29:15 +0100

    add metric for worker lambda processing rate
    - refactor sender request handler to move logging code into Logger class
    - capture the start time of the function
    - calculate the function processing rate using the function start time, end time and number of tokens
    - extend cloudwatch embedded metrics to include new metric for function processing rate
    - update local run to facilitate testing of embedded metrics

--- notificationworkerlambda/src/main/scala/com/gu/notifications/worker/NotificationWorkerLocalRun.scala
+++ notificationworkerlambda/src/main/scala/com/gu/notifications/worker/NotificationWorkerLocalRun.scala
@@ -11,7 +11,7 @@ import play.api.libs.json.Json
 
 import java.util.UUID
 import scala.jdk.CollectionConverters._
-
+import java.time.Instant
 object NotificationWorkerLocalRun extends App {
   val notification = BreakingNewsNotification(
     id = UUID.fromString("068b3d2b-dc9d-482b-a1c9-bd0f5dd8ebd7"),
@@ -41,7 +41,7 @@ object NotificationWorkerLocalRun extends App {
     val event = new SQSEvent()
     val sqsMessage = new SQSMessage()
     sqsMessage.setBody(Json.stringify(Json.toJson(tokens)))
-    sqsMessage.setAttributes((Map("SentTimestamp" -> "10").asJava))
+    sqsMessage.setAttributes((Map("SentTimestamp" -> s"${Instant.now.toEpochMilli}").asJava))
     event.setRecords(List(sqsMessage).asJava)
     event
   }
