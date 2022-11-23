7440690378ef73fbd0ecf95174a50463390274c6
Jacob Winch <jacob.winch@guardian.co.uk>
2022-11-23 15:19:36 +0000

    Add boilerplate for obtaining load balancer metrics

--- cdk/lib/registration.ts
+++ cdk/lib/registration.ts
@@ -1,7 +1,10 @@
 import { join } from 'path';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import { GuErrorBudgetAlarmExperimental } from '@guardian/cdk/lib/experimental/constructs';
 import type { App } from 'aws-cdk-lib';
+import { MathExpression, Metric } from 'aws-cdk-lib/aws-cloudwatch';
+import type { CfnLoadBalancer } from 'aws-cdk-lib/aws-elasticloadbalancing';
 import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
 
 export class Registration extends GuStack {
@@ -14,8 +17,29 @@ export class Registration extends GuStack {
 		// Until this project has been fully migrated to GuCDK you should update the 'old' infrastructure by modifying
 		// the YAML file and then re-running the snapshot tests to confirm that the changes are being pulled through by
 		// CDK
-		new CfnInclude(this, 'YamlTemplate', {
+		const cfnTemplate = new CfnInclude(this, 'YamlTemplate', {
 			templateFile: yamlTemplateFilePath,
 		});
+
+		const registrationLoadBalancer = cfnTemplate.getResource(
+			'LoadBalancerToPrivateASG',
+		) as CfnLoadBalancer;
+
+		const loadBalancerName = registrationLoadBalancer.ref;
+
+		function loadBalancerMetric(metricName: string) {
+			return new Metric({
+				metricName,
+				namespace: 'AWS/ELB',
+				statistic: 'Sum',
+				dimensionsMap: {
+					LoadBalancerName: loadBalancerName,
+				},
+			});
+		}
+
+		const applicationServerErrors = loadBalancerMetric('HTTPCode_Backend_5XX');
+		const loadBalancerErrors = loadBalancerMetric('HTTPCode_ELB_5XX');
+		const allRequests = loadBalancerMetric('RequestCount');
 	}
 }
