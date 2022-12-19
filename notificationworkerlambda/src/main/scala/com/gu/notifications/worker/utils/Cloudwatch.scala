3fc2d7f5d1f50cf757f178a294d08e8b048dd8b6
Jacob Winch <jacob.winch@guardian.co.uk>
2022-12-19 10:53:47 +0000

    Remove TODO which has now been completed

--- notificationworkerlambda/src/main/scala/com/gu/notifications/worker/utils/Cloudwatch.scala
+++ notificationworkerlambda/src/main/scala/com/gu/notifications/worker/utils/Cloudwatch.scala
@@ -70,7 +70,6 @@ class CloudwatchImpl(val senderMetricNs: String) extends Cloudwatch {
   def sendLatencyMetrics(shouldPushMetricsToAws: Boolean, stage: String, platform: Option[Platform]): Pipe[IO, List[Long], Unit] = _.evalMap { deliveryTimes =>
     IO.delay {
       val latencyMetrics = LatencyMetrics.aggregateForCloudWatch(deliveryTimes)
-      // TODO: split into batches of 150 unique values: https://docs.aws.amazon.com/AmazonCloudWatch/latest/APIReference/API_MetricDatum.html
       val dimension = new Dimension().withName("platform").withValue(platform.map(_.toString).getOrElse("unknown"))
       val requests = latencyMetrics.map { valuesAndCounts =>
         val cloudWatchMetric: MetricDatum = latencyDatum("TokenDeliveryLatency", valuesAndCounts.uniqueValues, valuesAndCounts.orderedCounts, dimension)
