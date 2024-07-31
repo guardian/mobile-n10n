09d4888d7aa3a7fb3a611ee31d74d082b6f4ea9d
Wai Sing Yiu <wai.sing.yiu@guardian.co.uk>
2024-07-31 11:03:28 +0100

    Change

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -5,6 +5,7 @@ import { Registration } from '../lib/registration';
 import { RegistrationsDbProxy } from '../lib/registrations-db-proxy';
 import { SloMonitoring } from '../lib/slo-monitoring';
 
+
 const app = new App();
 
 export const registrationCodeProps: GuStackProps = {
