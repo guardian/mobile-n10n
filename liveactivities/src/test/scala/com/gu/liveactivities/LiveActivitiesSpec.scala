1a46fcc5cbf267767e6cc63e9b6897eec72f95c9
Wai Sing Yiu <89925410+waisingyiu@users.noreply.github.com>
2026-03-17 09:58:49 +0000

    Add an empty test case

---
+++ liveactivities/src/test/scala/com/gu/liveactivities/LiveActivitiesSpec.scala
@@ -0,0 +1,17 @@
+package com.gu.liveactivities
+
+import java.net.URI
+import java.time.ZonedDateTime
+import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
+import pa.{Competition, MatchDay, MatchDayTeam, Round, Stage, Venue}
+
+class LiveActivitiesSpec extends Specification {
+
+  "A LiveActivities" should {
+
+    "create a channel" in {
+      1 must beEqualTo(1)
+    }
+  }
+}
