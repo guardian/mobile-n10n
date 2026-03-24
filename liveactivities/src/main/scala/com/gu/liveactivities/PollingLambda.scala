5024f7e2ef89251a917077146cd5b6ed09fbcb18
vanessa <32312712+vlbee@users.noreply.github.com>
2026-03-24 10:21:40 +0000

    test eventbridge put event

---
+++ liveactivities/src/main/scala/com/gu/liveactivities/PollingLambda.scala
@@ -0,0 +1,58 @@
+package com.gu.liveactivities
+
+import com.amazonaws.services.lambda.runtime.Context
+import software.amazon.awssdk.services.eventbridge.EventBridgeClient
+import software.amazon.awssdk.services.eventbridge.model.{
+  PutEventsRequest,
+  PutEventsRequestEntry
+}
+
+import scala.util.Try
+
+object PollingLiveGamesDataLambda {
+
+  private val eventBusName = "liveactivities-eventbus-CODE" // TODO - move to config
+  private val eventBridgeClient = EventBridgeClient.builder().build() // credentials? is cdk policy roll enough?
+
+  def handleRequest(context: Context): Unit = {
+
+    // TODO
+    // schedule lambda to run every 15seconds via cdk.
+    // Step 1 read liveActivity channel mapping dynamo table to get any match ids that are live.
+    // live match no? - do nothing.
+    // live match yes?
+    // Step 2 - publish event to event bridge with match id
+    // - collect unique competition ids for live matches
+    // - call PA /liveGames for all those competition ids that have live matches
+    // - send event to eventbus with match id and channel id.
+
+    // iteration:  tbc diff pa events to check if we need to send event or not??
+
+    val result = Try {
+
+      val entry = PutEventsRequestEntry
+        .builder()
+        .source("pa-live-game-updates")
+        .detailType("football-live-game")
+        .detail(
+          """{"matchId": "12345", "status": "live", "homeScore": 1, "awayScore": 0}""" // dummy data PA data will go heare
+        )
+        .eventBusName(eventBusName)
+        .build()
+
+      val request = PutEventsRequest
+        .builder()
+        .entries(entry)
+        .build()
+
+      val response = eventBridgeClient.putEvents(request)
+      println(
+        s"Event published. Failed entry count: ${response.failedEntryCount()}"
+      )
+    }
+
+    result.failed.foreach(e =>
+      println(s"Failed to publish event: ${e.getMessage}")
+    )
+  }
+}
