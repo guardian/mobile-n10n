88642f918877aab237a447c3cb1aad568835f463
vanessa <32312712+vlbee@users.noreply.github.com>
2026-04-28 13:33:04 +0100

    Tidy LiveActivityPayload model

--- api-models/src/main/scala/com/gu/mobile.notifications.client/models/liveActitivites/LiveActivityPayloads.scala
+++ api-models/src/main/scala/com/gu/mobile.notifications.client/models/liveActitivites/LiveActivityPayloads.scala
@@ -1,8 +1,9 @@
 package com.gu.mobile.notifications.client.models.liveActitivites
 
-import com.gu.mobile.notifications.client.models.{Payload, Topic}
-import play.api.libs.json.{JsString, Json, Writes}
 import java.util.UUID
+
+import play.api.libs.json._
+import com.gu.mobile.notifications.client.models.Payload
 
 /**
  * These are the models for eventbus payloads shared between live activity services.
@@ -10,8 +11,26 @@ import java.util.UUID
  * Channel Manager and Broadcast lambdas.
  **/
 
+case class EventBridgeEvent(
+  version: String,
+  id: String,
+  `detail-type`: String,
+  source: String,
+  account: String,
+  time: String,
+  region: String,
+  resources: List[String],
+  detail: LiveActivityPayload
+)
 
-// life cycle of a live activity:
+object EventBridgeEvent {
+  implicit val eventBridgeEventFormat: Format[EventBridgeEvent] = Json.format[EventBridgeEvent]
+}
+
+// todo codify detail-type strings
+
+
+// Life cycle of a live activity:
 sealed trait LiveActivityEventType
 case object CreateChannelEvent extends LiveActivityEventType
 case object StartLiveActivityEvent extends LiveActivityEventType
@@ -24,43 +43,55 @@ case object FootballLiveActivity extends LiveActivityType
 // tbc cricket, elections
 
 case class dynamoData(
-    liveActivityId: String,
-    isLive: Boolean,
-    data: Option[String], // tbc
-    competitionId: Option[String], // should this move to data??
-    lastEventId: Option[String],
-    lastEventAt: Option[Long]
+  liveActivityId: String,
+  isLive: Boolean,
+  data: Option[String], // tbc
+  competitionId: Option[String], // should this move to data??†
+  lastEventId: Option[String],
+  lastEventAt: Option[Long]
 )
 
 case class LiveActivityPayload(
-    id: UUID, // unique event id for each payload associate with a live activity, used for de-duplication
-    eventType: LiveActivityEventType,
-    liveActivityType: LiveActivityType,
-    liveActivityID: String, // Match ID in the case of football, tbc for other sports/events
-    dynamoStoreData: Option[
-      String
-    ], // data not in contentstate but specific to match, election, etc TBC if this is needed.
-    broadcastContentStateData: Option[ContentState],
-    eventTimestamp: Long,
-    topics: List[Topic] // we will need to know topics for push to start
+  id: UUID, // unique event id for each payload associate with a live activity, used for de-duplication
+  eventType: LiveActivityEventType,
+  liveActivityType: LiveActivityType,
+  liveActivityID: String, // Match ID in the case of football, tbc for other sports/events
+  dynamoStoreData: Option[String], // data not in contentstate but specific to match, election, etc TBC if this is needed.
+  broadcastContentStateData: Option[ContentState],
+  eventTimestamp: Long,
+//  topics: List[Topic] // we will need to know topics for push to start
 ) extends Payload
 
 object LiveActivityPayload {
-  implicit val liveActivityEventTypeWrites: Writes[LiveActivityEventType] =
+
+  implicit val liveActivityTypeFormat: Format[LiveActivityType] = Format(
+    Reads {
+      case JsString("FootballLiveActivity") => JsSuccess(FootballLiveActivity)
+      case _                                => JsError("Unknown LiveActivityType")
+    },
     Writes {
-      case CreateChannelEvent     => JsString("CreateChannel")
+      case FootballLiveActivity => JsString("FootballLiveActivity")
+    }
+  )
+
+  implicit val liveActivityEventTypeFormat: Format[LiveActivityEventType] = Format(
+    Reads {
+      case JsString("CreateChannel")      => JsSuccess(CreateChannelEvent)
+      case JsString("StartLiveActivity")  => JsSuccess(StartLiveActivityEvent)
+      case JsString("UpdateLiveActivity") => JsSuccess(UpdateLiveActivityEvent)
+      case JsString("EndLiveActivity")    => JsSuccess(EndLiveActivityEvent)
+      case JsString("DeleteChannel")      => JsSuccess(DeleteChannelEvent)
+      case _                              => JsError("Unknown LiveActivityEventType")
+    },
+    Writes {
+      case CreateChannelEvent      => JsString("CreateChannel")
       case StartLiveActivityEvent  => JsString("StartLiveActivity")
       case UpdateLiveActivityEvent => JsString("UpdateLiveActivity")
       case EndLiveActivityEvent    => JsString("EndLiveActivity")
       case DeleteChannelEvent      => JsString("DeleteChannel")
     }
+  )
 
-  implicit val liveActivityTypeWrites: Writes[LiveActivityType] = Writes {
-    case FootballLiveActivity => JsString("FootballLiveActivity")
-  }
-
-  implicit val dynamoDataWrites: Writes[dynamoData] = Json.writes[dynamoData]
-
-  implicit val liveActivityPayloadWrites: Writes[LiveActivityPayload] =
-    Json.writes[LiveActivityPayload]
+  implicit val liveActivityPayloadFormat: Format[LiveActivityPayload] = Json.format[LiveActivityPayload]
+  implicit val dynamoDataFormat: Format[dynamoData] = Json.format[dynamoData]
 }
