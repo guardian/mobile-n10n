99432ac70c401ed2e0a60b3fd7091250ce0bf865
vanessa <32312712+vlbee@users.noreply.github.com>
2026-05-13 13:33:15 +0100

    Fix dynamoDb spec set up

--- liveactivities/src/test/scala/com/gu/liveactivities/DynamodbSpecification.scala
+++ liveactivities/src/test/scala/com/gu/liveactivities/DynamodbSpecification.scala
@@ -1,24 +1,14 @@
 package com.gu.liveactivities
 
-import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient
-import software.amazon.awssdk.auth.credentials.{
-	AwsCredentialsProviderChain,
-	ProfileCredentialsProvider,
-	DefaultCredentialsProvider,
-}
-import software.amazon.awssdk.regions.Region.EU_WEST_1
 import org.specs2.mutable.Specification
-import org.specs2.specification.{BeforeAfterAll, Scope}
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import org.specs2.specification.{BeforeAfterAll,BeforeAfterEach, Scope}
 import software.amazon.awssdk.services.dynamodb.model._
-import java.net.URI
-import software.amazon.awssdk.regions.Region
+
 import org.scanamo.ScanamoAsync
 import scala.concurrent.ExecutionContext
 import org.scanamo.LocalDynamoDB
 
-trait DynamodbSpecification extends Specification with BeforeAfterAll {
+trait DynamodbSpecification extends Specification with BeforeAfterAll with BeforeAfterEach {
 
   sequential
 
@@ -31,13 +21,18 @@ trait DynamodbSpecification extends Specification with BeforeAfterAll {
   val TestEndpoint = "http://localhost:8002"
 
   override def beforeAll(): Unit = {
-    LocalDynamoDB.createTable(awsClient)(TableName)(targetTableAttributes: _*)   
+    LocalDynamoDB.createTable(awsClient)(TableName)(targetTableAttributes: _*)
   }
+
+  override def before: Unit = {
+    LocalDynamoDB.deleteTable(awsClient)(TableName)
+    LocalDynamoDB.createTable(awsClient)(TableName)(targetTableAttributes: _*)
+  }
+  override def after: Unit = ()
 
   override def afterAll(): Unit = {
     LocalDynamoDB.deleteTable(awsClient)(TableName)
   }
-
   private def awsClient = LocalDynamoDB.client(8002)
 
   private def scanamo = ScanamoAsync(awsClient)
