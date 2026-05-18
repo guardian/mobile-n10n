58a55ea1e798d540f84c8d88bd10d4d3998919eb
vanessa <32312712+vlbee@users.noreply.github.com>
2026-05-18 11:17:18 +0100

    Only log once the list of unsupported competitions each time.

--- football/src/main/scala/com/gu/mobile/notifications/football/lib/FootballData.scala
+++ football/src/main/scala/com/gu/mobile/notifications/football/lib/FootballData.scala
@@ -15,57 +15,58 @@ import scala.util.{Failure, Success, Try}
 case class EndedMatch(matchId: String, startTime: DateTime)
 
 case class PACompetition(
-                        id: String,
-                        tag: String,
-                        fullName: String,
-                        shortName: String,
-                        startDate: Option[LocalDate] = None,
-                        endDate: Option[LocalDate] = None,
-                      )
+    id: String,
+    tag: String,
+    fullName: String,
+    shortName: String,
+    startDate: Option[LocalDate] = None,
+    endDate: Option[LocalDate] = None,
+)
 
 object PACompetition {
   implicit val competitionFormat: Format[PACompetition] = Json.format[PACompetition]
 }
 
 class FootballData(
-                    paClient: PaFootballClient,
-                    syntheticEvents: SyntheticMatchEventGenerator,
-                    competitionsDataStore: S3DataStore[PACompetition],
-                    stage: String
+    paClient: PaFootballClient,
+    syntheticEvents: SyntheticMatchEventGenerator,
+    competitionsDataStore: S3DataStore[PACompetition],
+    stage: String,
 ) extends Logging {
 
   implicit class RichMatchDay(matchDay: MatchDay) {
 
     private lazy val internationalTeamsForFriendlies = Set(
-      "497",    //England,
-      "499",    //'Scotland,
-      "630",    //Wales,
-      "494",    //Republic of Ireland,
-      "964",    //Northern Ireland,
-      "999",    //Spain,
-      "1678",   //Germany,
-      "717",    //Italy,
-      "619",    //France,
-      "997",    //Belgium,
-      "5539",   //Portugal,
-      "1661",   //Turkey,
-      "629",    //Poland,
-      "716",    //Norway,
-      "5845",   //Sweden,
-      "986",    //Denmark,
-      "5827",   //Russia,
-      "965",    //Argentina,
-      "23104"   //Brazil
+      "497", // England,
+      "499", // 'Scotland,
+      "630", // Wales,
+      "494", // Republic of Ireland,
+      "964", // Northern Ireland,
+      "999", // Spain,
+      "1678", // Germany,
+      "717", // Italy,
+      "619", // France,
+      "997", // Belgium,
+      "5539", // Portugal,
+      "1661", // Turkey,
+      "629", // Poland,
+      "716", // Norway,
+      "5845", // Sweden,
+      "986", // Denmark,
+      "5827", // Russia,
+      "965", // Argentina,
+      "23104", // Brazil
     )
 
-    lazy val isUncoveredInternationalFriendly: Boolean = Set(matchDay.homeTeam.id, matchDay.awayTeam.name).intersect(internationalTeamsForFriendlies).isEmpty
+    lazy val isUncoveredInternationalFriendly: Boolean =
+      Set(matchDay.homeTeam.id, matchDay.awayTeam.name).intersect(internationalTeamsForFriendlies).isEmpty
 
     lazy val isEarlyQualifyingRound: Boolean = Try(matchDay.round.roundNumber.toInt) match {
       case Success(r) => r < 3
-      case _ => false
+      case _          => false
     }
   }
-  
+
   def pollFootballData(dateTime: ZonedDateTime): Future[List[RawMatchData]] = {
     logger.info("Starting poll for new match events")
 
@@ -86,7 +87,6 @@ class FootballData(
     def inProgress(m: MatchDay): Boolean =
       m.date.minusHours(2).isBefore(dateTime) && m.date.plusHours(4).isAfter(dateTime)
 
-
     // unfortunately PA provide 00:00 as start date when they don't have the start date
     // so we can't do anything with these matches
     def isMidnight(matchDay: MatchDay): Boolean = {
@@ -94,40 +94,60 @@ class FootballData(
       localDate.getHour() == 0 && localDate.getMinute() == 0
     }
 
-    //Theres some stuff that PA don't provide data for and we want to supress alerts for these matches
-    def paProvideAlerts(matchDay: MatchDay) : Boolean = {
-      matchDay.competition.map {
-        c => c.id match {
-          //International friendly: Must involve at least one of a whitelisted set of teams
-          case "721" if matchDay.isUncoveredInternationalFriendly => false
-          //FA cup qualifying rounds not covererd before round 3
-          case "303" if matchDay.isEarlyQualifyingRound => false
-          case _ => true
+    // Theres some stuff that PA don't provide data for and we want to supress alerts for these matches
+    def paProvideAlerts(matchDay: MatchDay): Boolean = {
+      matchDay.competition
+        .map { c =>
+          c.id match {
+            // International friendly: Must involve at least one of a whitelisted set of teams
+            case "721" if matchDay.isUncoveredInternationalFriendly => false
+            // FA cup qualifying rounds not covererd before round 3
+            case "303" if matchDay.isEarlyQualifyingRound => false
+            case _                                        => true
+          }
         }
-      }.getOrElse(false) //Shouldn't ever happen
+        .getOrElse(false) // Shouldn't ever happen
     }
 
-    def competitionIsSupported(supportedCompetitions: List[PACompetition])(matchDay: MatchDay): Boolean =
-      matchDay.competition.exists { matchComp =>
-        val isSupported = supportedCompetitions.exists(_.id == matchComp.id)
-        if (!isSupported) logger.warn(s"match ${matchDay.id} from competition ${matchComp.id} is not supported")
-        isSupported
+    def competitionIsSupported(
+        matches: List[MatchDay],
+        supportedCompetitions: List[PACompetition],
+    ): List[MatchDay] = {
+      if (supportedCompetitions.isEmpty) then {
+        logger.warn("No supported competitions PA list retrieved, assuming all matches are supported")
+        return matches
       }
+      else {
+        val (supported, unsupported) = matches.partition { m =>
+          m.competition.exists(comp => supportedCompetitions.exists(_.id == comp.id))
+        }
+        if (unsupported.nonEmpty) {
+          val msg = unsupported
+            .map(m => s"${m.id} from competition ${m.competition.map(_.id).getOrElse("unknown")}")
+            .mkString(", ")
+          logger.warn(s"Unsupported matches: $msg")
+        }
+        supported
+      }
+    }
 
     logger.info(s"Retrieving matches on or around $dateTime from PA")
     for {
-       matches <- paClient.aroundToday(dateTime)
-       competitions <- competitionsDataStore
-         .fetch(s"${stage}/competition/competitions.json")
-         .recover { case exception =>
-           // We don't want to fail the whole process if we can't retrieve the list of competitions,
-           // we'll just assume all competitions are supported and log the error
-           logger.error(s"Failed to retrieve list of competitions: ${exception.getMessage}", exception)
-           List.empty[PACompetition]
-         }
+      matches <- paClient.aroundToday(dateTime)
+      competitions <- competitionsDataStore
+        .fetch(s"${stage}/competition/competitions.json")
+        .recover { case exception =>
+          // We don't want to fail the whole process if we can't retrieve the list of competitions,
+          // we'll just assume all competitions are supported and log the error
+          logger.error(s"Failed to retrieve list of competitions: ${exception.getMessage}", exception)
+          List.empty[PACompetition]
+        }
+      matchesInSupportedCompetitions = competitionIsSupported(matches, competitions)
+      _ = logger.info(
+        s"Retrieved ${matches.size} matches from PA, ${matchesInSupportedCompetitions.size} are in supported competitions",
+      )
     } yield {
-      matches
-        .filter(m => competitions.isEmpty || competitionIsSupported(competitions)(m))
+      matchesInSupportedCompetitions
         .filter(inProgress)
         .filter(paProvideAlerts)
         .filterNot(isMidnight)
