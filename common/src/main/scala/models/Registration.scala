cb6c66bb81aaebc2761e227e0d7dba1f49139ed9
Ara Cho <jarjeb@gmail.com>
2026-06-12 15:10:51 +0100

    filter out invalid topics

--- common/src/main/scala/models/Registration.scala
+++ common/src/main/scala/models/Registration.scala
@@ -1,6 +1,7 @@
 package models
 
-import play.api.libs.json.{Format, Json}
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 case class Registration(
   deviceToken: DeviceToken,
@@ -11,5 +12,22 @@ case class Registration(
 )
 
 object Registration {
-  implicit val registrationJF: Format[Registration] = Json.format[Registration]
+  private val validTopicsReads: Reads[Set[Topic]] = Reads { json =>
+    json.validate[JsArray].map { arr =>
+      arr.value.flatMap(_.validate[Topic].asOpt).toSet
+    }
+  }
+
+  private val validTopicsFormat: Format[Set[Topic]] = Format(
+    validTopicsReads,
+    Writes[Set[Topic]](topics => Json.toJson(topics.toList))
+  )
+
+  implicit val registrationJF: Format[Registration] = (
+    (__ \ "deviceToken").format[DeviceToken] and
+    (__ \ "platform").format[Platform] and
+    (__ \ "topics").format[Set[Topic]](validTopicsFormat) and
+    (__ \ "buildTier").formatNullable[String] and
+    (__ \ "appVersion").formatNullable[String]
+  )(Registration.apply, r => (r.deviceToken, r.platform, r.topics, r.buildTier, r.appVersion))
 }
