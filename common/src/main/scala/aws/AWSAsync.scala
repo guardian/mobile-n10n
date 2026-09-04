8e3afc1f1064e1eb38c6fcc35e1f1994c76bee92
vanessa <32312712+vlbee@users.noreply.github.com>
2026-09-04 10:53:18 +0100

    Migrate Notification to aws SQS sdk v2

--- common/src/main/scala/aws/AWSAsync.scala
+++ common/src/main/scala/aws/AWSAsync.scala
@@ -2,7 +2,7 @@ package aws
 
 import com.amazonaws.AmazonWebServiceRequest
 import com.amazonaws.handlers.AsyncHandler
-import java.util.concurrent.{Future => JFuture}
+import java.util.concurrent.{CompletableFuture, Future => JFuture}
 import scala.concurrent.{Future, Promise}
 
 object AWSAsync {
@@ -12,6 +12,7 @@ object AWSAsync {
       override def onSuccess(request: Request, result: Result): Unit = { p.success(result); () }
     }
 
+  // todo delete callback when v1 migration complete - common/dynamo
   @inline
   def wrapAsyncMethod[Request <: AmazonWebServiceRequest, Result](
     f: (Request, AsyncHandler[Request, Result]) => JFuture[Result],
@@ -19,6 +20,22 @@ object AWSAsync {
   ): Future[Result] = {
     val p = Promise[Result]()
     f(request, promiseToAsyncHandler(p))
+    p.future
+  }
+
+  @inline
+  def wrapCompletableFuture[T](cf: CompletableFuture[T]): Future[T] = {
+    val p = Promise[T]()
+    cf.whenComplete { (result, exception) =>
+      if (exception != null) {
+        p.failure(exception)
+        ()
+      }
+      else {
+        p.success(result);
+        ()
+      }
+    }
     p.future
   }
 }
